b540afa8858317398618d29be12bfad4d34c90c5
lokenbista <lokenbista@192.168.1.2>
2026-06-27 10:25:09 +0545

    Initial Commit

---
+++ Backend/src/main/java/we/link/BackendApplication.java
@@ -0,0 +1,13 @@
+package we.link;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
